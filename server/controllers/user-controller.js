79c22206239036c1d003b186b0386f2d1b550dac
Miguel319 <miguelp90.319@gmail.com>
2019-12-22 22:02:33 -0400

    Password hash & salt + Mongoose err handling

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,74 +1,93 @@
-const User = require('../models/user-model');
+const User = require("../models/user-model");
+const { normalizeErrors } = require("../helpers/mongoose");
 
 exports.signin = (req, res) => {};
 
 exports.signup = (req, res) => {
-  const {
-    username,
-    email,
-    password,
-    passwordConfirmation
-  } = req.body;
+  const { username, email, password, passwordConfirmation } = req.body;
 
   if (!email || !password) {
     return res.status(400).send({
-      errors: [{
-        title: 'Missing data',
-        detail: 'The email and password fields are required.'
-      }]
-    });
-  }
-
-  if (password !== passwordConfirmation) {
-    return res.status(400).send({
-      errors: [{
-        title: 'Invalid password',
-        detail: 'Both password must be equal.'
-      }]
+      errors: [
+        {
+          title: "Missing data",
+          detail: "The email and password fields are required."
+        }
+      ]
     });
   }
 
   if (username && username.length < 4) {
     return res.status(400).send({
-      errors: [{
-        title: 'Username is too short',
-        detail: 'The username must have at least 4 characters.'
-      }]
+      errors: [
+        {
+          title: "Username is too short",
+          detail: "The username must have at least 4 characters."
+        }
+      ]
     });
   }
 
-  User.findOne({
-    email
-  }, (err, data) => {
-    if (err) {
-      return res.status(422).send({
-        // TODO: Handle Mongoose error
-      });
-    }
-
-    if (data) {
-      return res.status(422).send({
-        errors: [{
-          title: 'Invalid email.',
-          detail: 'There\'s already an account with email.'
-        }]
-      });
-    }
-
-    const user = new User({
-      username,
-      email,
-      password
+  if (password.length < 4) {
+    return res.status(400).send({
+      errors: [
+        {
+          title: "Password is too short",
+          detail: "The password must have at least 4 characters."
+        }
+      ]
     });
+  }
 
-    user.save((err) => {
+  if (password !== passwordConfirmation) {
+    return res.status(422).send({
+      errors: [
+        {
+          title: "Invalid password",
+          detail: "Both password must be equal."
+        }
+      ]
+    });
+  }
+
+  User.findOne(
+    {
+      email
+    },
+    (err, data) => {
       if (err) {
-        // TODO: Handle Mongoose error
+        return res
+          .status(422)
+          .send({ errors: normalizeErrors(err.errors) });
       }
 
-      res.json({
-        registered: true
+      if (data) {
+        return res.status(422).send({
+          errors: [
+            {
+              title: "Invalid email.",
+              detail: "There's already an account with email."
+            }
+          ]
+        });
+      }
+
+      const user = new User({
+        username,
+        email,
+        password
       });
-    })
-  });
-}
+
+      user.save(err => {
+        if (err)
+          return res
+            .status(422)
+            .send({ errors: normalizeErrors(err.errors) });
+
+        return res.json({
+          registered: true
+        });
+      });
+    }
+  );
+};
--- server/seeder/dummy-db.js
+++ server/seeder/dummy-db.js
@@ -1,64 +1,112 @@
-const Rental = require('../models/rentals-model.js');
-
-class DummyDb {
-  rentals;
-
-  constructor() {
-    this.createRentals();
-  }
-
-  createRentals() {
-    this.rentals = [{
-        title: "Nice view on ocean",
-        city: "San Francisco",
-        street: "Main street",
-        category: "condo",
-        image: "https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg",
-        bedrooms: 4,
-        shared: true,
-        description: "Very nice apartment in center of the city.",
-        dailyRate: 43
-      },
-      {
-        title: "Modern apartment in center",
-        city: "New York",
-        street: "Time Square",
-        category: "apartment",
-        image: "https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg",
-        bedrooms: 1,
-        shared: false,
-        description: "Very nice apartment in center of the city.",
-        dailyRate: 11
-      },
-      {
-        title: "Old house in nature",
-        city: "Spisska Nova Ves",
-        street: "Banicka 1",
-        category: "house",
-        image: "https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg",
-        bedrooms: 5,
-        shared: true,
-        description: "Very nice apartment in center of the city.",
-        dailyRate: 23
-      }
-    ];
-  }
-
-  async cleanDb() {
-    await Rental.deleteMany();
-  }
-
-  addRentalsToDb() {
-    for (let rental of this.rentals) {
-      const newRental = new Rental(rental);
-      newRental.save();
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
-  }
-
-  async seedToDb() {
-    await this.cleanDb();
-    this.addRentalsToDb();
-  }
-}
-
+};
+var Rental = require('../models/rentals-model.js');
+var DummyDb = /** @class */ (function () {
+    function DummyDb() {
+        this.createRentals();
+    }
+    DummyDb.prototype.createRentals = function () {
+        this.rentals = [{
+                title: "Nice view on ocean",
+                city: "San Francisco",
+                street: "Main street",
+                category: "condo",
+                image: "https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg",
+                bedrooms: 4,
+                shared: true,
+                description: "Very nice apartment in center of the city.",
+                dailyRate: 43
+            },
+            {
+                title: "Modern apartment in center",
+                city: "New York",
+                street: "Time Square",
+                category: "apartment",
+                image: "https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg",
+                bedrooms: 1,
+                shared: false,
+                description: "Very nice apartment in center of the city.",
+                dailyRate: 11
+            },
+            {
+                title: "Old house in nature",
+                city: "Spisska Nova Ves",
+                street: "Banicka 1",
+                category: "house",
+                image: "https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg",
+                bedrooms: 5,
+                shared: true,
+                description: "Very nice apartment in center of the city.",
+                dailyRate: 23
+            }
+        ];
+    };
+    DummyDb.prototype.cleanDb = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, Rental.deleteMany()];
+                    case 1:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    DummyDb.prototype.addRentalsToDb = function () {
+        for (var _i = 0, _a = this.rentals; _i < _a.length; _i++) {
+            var rental = _a[_i];
+            var newRental = new Rental(rental);
+            newRental.save();
+        }
+    };
+    DummyDb.prototype.seedToDb = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, this.cleanDb()];
+                    case 1:
+                        _a.sent();
+                        this.addRentalsToDb();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    return DummyDb;
+}());
 module.exports = DummyDb;
