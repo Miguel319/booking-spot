5b79d173d51b1af02d07103c4495e31ecadbde70
Miguel319 <miguelp90.319@gmail.com>
2019-12-29 20:09:27 -0400

    Booking validations & Rental refactoring

--- server/controllers/booking.js
+++ server/controllers/booking.js
@@ -3,6 +3,7 @@ const Booking = require("../models/booking");
 const Rental = require("../models/rental");
 const User = require("../models/user");
 const ErrorResponse = require("../helpers/errorResponse");
+const moment = require("moment");
 
 exports.createBooking = asyncHandler(async (req, res, next) => {
   const { startAt, endAt, totalPrice, guests, days, rental } = req.body;
@@ -13,7 +14,7 @@ exports.createBooking = asyncHandler(async (req, res, next) => {
   await Rental.findById(rental._id)
     .populate("bookings")
     .populate("user")
-    .exec((err, rentalRes) => {
+    .exec(async (err, rentalRes) => {
       if (err) {
         return sendErrResponse(
           "Rental not found",
@@ -27,18 +28,57 @@ exports.createBooking = asyncHandler(async (req, res, next) => {
         return sendErrResponse(
           "Invalid operation",
           "You can't book your own rental.",
+          401,
+          null,
+          res
+        );
+      }
+
+      if (!areDatesValid(booking)) {
+        return sendErrResponse(
+          "Invalid dates",
+          "The end date must be greater than or equal to start date.",
           400,
           null,
           res
         );
       }
 
-      // TODO: Check if booking is valid
+      if (!areDatesAvailable(booking, rentalRes)) {
+        return sendErrResponse(
+          "Invalid operation",
+          "Choosen dates are already taken.",
+          400,
+          null,
+          res
+        );
+      }
 
-      res.status(200).json({
+      booking.user = user;
+      booking.rental = rentalRes;
+
+      rentalRes.bookings.push(booking);
+
+      try {
+        await booking.save();
+        await User.updateOne(
+          { _id: user.id },
+          { $push: { bookings: booking } }
+        );
+      } catch {
+        return sendErrResponse(
+          "Unable to perform operation",
+          "Unable to save booking. Please try again.",
+          400
+        );
+      }
+
+      rentalRes.save();
+
+      return res.status(200).json({
         success: true,
-        booking,
-        rentalRes
+        startAt,
+        endAt
       });
     });
 });
@@ -53,4 +93,28 @@ const sendErrResponse = (title, message, statusCode, next, res = null) => {
       message: message
     });
   }
+};
+
+const areDatesValid = proposedBooking =>
+  moment(proposedBooking.endAt) >= moment(proposedBooking.startAt);
+
+const areDatesAvailable = (proposedBooking, rental) => {
+  let valid = true;
+
+  if (rental.bookings) {
+    valid = rental.bookings.every(booking => {
+      const proposedStart = moment(proposedBooking.startAt);
+      const proposedEnd = moment(proposedBooking.endAt);
+
+      const start = moment(booking.startAt);
+      const end = moment(booking.endAt);
+
+      return (
+        (start < proposedStart && end < proposedStart) ||
+        (proposedEnd < end && proposedEnd < start)
+      );
+    });
+  }
+
+  return valid;
 };
